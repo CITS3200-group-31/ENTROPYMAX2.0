c362dca3f980e61332ac3367cbf151dca60486f9
MonkeInABox <jezb03@outlook.com>
2025-08-25 13:34:49 +0800

    feature/parquet_to_dict

--- frontend/utils/get_data_from_groupings.py
+++ frontend/utils/get_data_from_groupings.py
@@ -9,25 +9,21 @@ Dictionary Setup:
 └────────────────┘      └─────────────────────────────────────────────┘
 """
 
-import csv
+# import csv
+import pyarrow.parquet as pq
 
 group_data_dict = {}
 
 def create_data(input_file):
-    with open(input_file, newline = '', encoding = 'utf-8') as file:
-        reader = csv.reader(file)
-        next(reader)
-        next(reader)
-        next(reader)
-        next(reader)
-        next(reader)
-        headers = next(reader)
-        i = 1
-        # need to figure out how to get group headings
-        for row in reader:
-            if len(row) < 74: continue
-            i = 0
-            add_group_data(2, row[0], row[1:73], headers[1:73], row[1])
+    reader = pq.read_table(input_file)
+    # print(reader)
+    i = 1
+    # need to figure out how to get group heading
+    print(reader.num_rows)
+    for row in range(reader.num_rows):
+        i = 0
+        # currently making all in the grouping number of 2
+        add_group_data(2, reader[0][row], [reader[i][row] for i in range(2,70)], [reader[i][0] for i in range(2,70)], reader[1][row])
             
 
 def add_group_data(group_count, group_index, x, y, sample_id):
@@ -40,7 +36,8 @@ def add_group_data(group_count, group_index, x, y, sample_id):
     if group_count not in group_data_dict:
         group_data_dict[group_count] = []
     group_data_dict[group_count].append(entry)
+    # print(group_data_dict[2][-1])
     
 
-create_data("example_data.csv.csv")
-print(group_data_dict.keys())
+create_data("frontend/utils/data2.parquet")
+#print(group_data_dict.keys())
